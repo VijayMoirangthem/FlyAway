d7f3f89c8aea4cdb6d57dd82369c1087bf5777d9
Vijay Moirangthem <73604885+VijayMoirangthem@users.noreply.github.com>
2022-04-11 22:19:27 +0530

    Project upload
    Phase 2

---
+++ src/main/java/com/simplearn/webapp/Flyaway/App.java
@@ -0,0 +1,13 @@
+package com.simplearn.webapp.Flyaway;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/simplearn/webapp/Flyaway/AppTest.java
@@ -0,0 +1,20 @@
+package com.simplearn.webapp.Flyaway;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
